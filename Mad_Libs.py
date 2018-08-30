4f9de5080e9135f29cba6c5ac8248a3db1d58e7f
ErikBatista <erikbatista42@gmail.com>
2018-08-29 18:26:00 -0700

    added more lines of code to make user feel understood

--- Mad_Libs.py
+++ Mad_Libs.py
@@ -1,2 +1,12 @@
-# Mad Libs about a person. This program's goal is to get to know who you are.
+# Mad Libs about a person. This program's goal is to get the computer to know who you are.
 
+print("We want to know who you are...")
+print("Answer the following questions so we can get to know you! ")
+
+firstName = input("What is your first name? ")
+age = input("How old are you? ")
+first_thing_you_do_for_fun = input("What do you do for fun? (say something like 'play video games') ")
+second_thing_you_do_for_fun = input("What's another thing you like to do for fun? ")
+
+print("Hey, my name is " + firstName + ". I am " + age + " years old. For fun, I like to " + first_thing_you_do_for_fun + " and " + second_thing_you_do_for_fun + ".")
+#     )
