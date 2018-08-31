a4591f18feee9f1fc816d956c912718110c1571c
ErikBatista <erikbatista42@gmail.com>
2018-08-31 11:32:08 -0700

    organized code more

--- Mad_Libs.py
+++ Mad_Libs.py
@@ -3,10 +3,25 @@
 print("We want to know who you are...")
 print("Answer the following questions so we can get to know you! ")
 
-firstName = input("What is your first name? ")
-age = input("How old are you? ")
-first_thing_you_do_for_fun = input("What do you do for fun? (say something like 'play video games') ")
-second_thing_you_do_for_fun = input("What's another thing you like to do for fun? ")
+mad_list = []
 
-print("Hey, my name is " + firstName + ". I am " + age + " years old. For fun, I like to " + first_thing_you_do_for_fun + " and " + second_thing_you_do_for_fun + ".")
-#     )
+firstName = input("What is your first name? ")
+mad_list.append(firstName)
+
+age = input("How old are you? ")
+mad_list.append(age)
+
+    
+first_thing_you_do_for_fun = input("What do you do for fun? (say something like 'play video games'")
+mad_list.append(first_thing_you_do_for_fun)
+
+second_thing_you_do_for_fun = input("What's another thing you like to do for fun? ")
+mad_list.append(second_thing_you_do_for_fun)
+
+# print(mad_list)
+
+
+def test():
+    print("Hey, my name is %s . I am %s years old. For fun, I like to %s and %s." % (firstName, age, first_thing_you_do_for_fun, second_thing_you_do_for_fun) )   
+
+test()
