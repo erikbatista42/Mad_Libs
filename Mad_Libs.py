d246f41170894e9160d101c447e480c30aa72f57
ErikBatista <erikbatista42@gmail.com>
2018-09-02 21:26:29 -0700

    added float

--- Mad_Libs.py
+++ Mad_Libs.py
@@ -45,10 +45,16 @@ mad_list.append(what_type_of_music_you_listen_to)
 one_thing_you_want_to_learn = input("What's one thing you want to learn? (example: drawing) ")
 mad_list.append(one_thing_you_want_to_learn)
 
+fav_shoe_brand = input("What's your favorite shoe brand? (example: Adidas)")
+mad_list.append(fav_shoe_brand)
+
+shoe_size = float(input("What's your shoe size?"))
+mad_list.append(shoe_size)
+
 # print(mad_list)
 # myTup = {"erik" : 18}
 
 def test():
-    print("Hey, my name is %s . I am %s years old. For fun, I like to %s and %s. My favorite color is %s. I have %s siblings. For work, I %s. My favorite place to be is %s. The closest person to me is %s. I'm from %s and I live in %s. I like to listen to %s. One thing I want to learn is %s." % (firstName, age, first_thing_you_do_for_fun, second_thing_you_do_for_fun, favColor, num_of_siblings, what_you_do_for_work, fav_place_to_be, name_of_closest_person, where_you_from, where_do_you_live, what_type_of_music_you_listen_to, one_thing_you_want_to_learn) )   
+    print("Hey, my name is %s . I am %s years old. For fun, I like to %s and %s. My favorite color is %s. I have %s siblings. For work, I %s. My favorite place to be is %s. The closest person to me is %s. I'm from %s and I live in %s. I like to listen to %s. One thing I want to learn is %s. My favorite shoe brand is %s. My shoe size is %s." % (firstName, age, first_thing_you_do_for_fun, second_thing_you_do_for_fun, favColor, num_of_siblings, what_you_do_for_work, fav_place_to_be, name_of_closest_person, where_you_from, where_do_you_live, what_type_of_music_you_listen_to, one_thing_you_want_to_learn, fav_shoe_brand, shoe_size) )   
 
 test()
