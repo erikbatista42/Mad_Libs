4e8136544b8b7e5915b4b00b77a71899c5d13636
luxury <erikbatista42@gmail.com>
2018-10-24 18:28:14 -0700

    completed mad libs for now. It's kind of short

---
+++ Mad_Libs.py
@@ -0,0 +1,16 @@
+welcomeMessage = "Hi! Let's play mad libs :-)"
+print(welcomeMessage)
+for dot in "." * 10:
+    print(dot)
+
+print("Give me ...")
+firstNoun = input("a noun: ")
+secondNoun = input("another noun: ")
+thirdNoun = input("a plural noun:")
+firstPlace = input("A place: ")
+firstAdjective = input("An adjective: ")
+
+madlibsTemplate = "I went to my {} and got {}. Then, I got a {} and put it in my {}. I wish someody could take care of my {} stuff and sleep with it. I will be really {} am I right?".format(firstNoun,thirdNoun,secondNoun,firstNoun,secondNoun,firstAdjective)
+print(madlibsTemplate)
+
+
