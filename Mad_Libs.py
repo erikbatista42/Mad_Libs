24d59811809e7333d37395731bcb76404f9d3202
luxury <erikbatista42@gmail.com>
2018-10-29 16:18:03 -0700

    refactored project

--- Mad_Libs.py
+++ Mad_Libs.py
@@ -1,17 +1,22 @@
-welcomeMessage = "Hi! Let's play mad libs :-)"
-print(welcomeMessage)
-for dot in "." * 10:
-    print(dot)
 
-print("Give me ...")
+def loadGame():
+    print("Hi! Let's play mad libs :-)")
 
-firstNoun = input("a noun: ")
-secondNoun = input("another noun: ")
-thirdNoun = input("a plural noun:")
-firstPlace = input("A place: ")
-firstAdjective = input("An adjective: ")
+    for dot in "." * 10:
+        print(dot)
 
-madlibsTemplate = "I went to my {} and got {}. Then, I got a {} and put it in my {}. I wish someody could take care of my {} stuff and sleep with it. I will be really {} am I right?".format(firstNoun,thirdNoun,secondNoun,firstNoun,secondNoun,firstAdjective)
-print(madlibsTemplate)
+loadGame()
 
+def play():
+    print("Give me ...")
 
+    firstNoun = input("a noun: ")
+    secondNoun = input("another noun: ")
+    thirdNoun = input("a plural noun:")
+    firstPlace = input("A place: ")
+    firstAdjective = input("An adjective: ")
+
+    madlibsTemplate = "I went to my {} and got {}. Then, I got a {} and put it in my {}. I wish someody could take care of my {} stuff and sleep with it. It will be really {} am I right?".format(firstNoun,thirdNoun,secondNoun,firstNoun,secondNoun,firstAdjective)
+    print(madlibsTemplate)
+
+play()
